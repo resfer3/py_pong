3953984c29060f2bfbc814a84597211787b19770
resfer3 <resfer333@gmail.com>
2024-07-28 19:11:55 -0600

    added player2 and added movement

--- game.py
+++ game.py
@@ -9,30 +9,48 @@ def main():
   running = True
   dt = 0
 
-  player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
   print(screen.get_width() / 2)
   print(screen.get_height() / 2)
 
   player1_rgb = (66, 176, 245)
   player2_rgb = (245, 66, 81)
 
-  # game-board
+  # game board
   rect = Rect(10 , 50, 1004, 708)
 
-  # player1
+  # collision object
+  collision_surface = Rect(10, 70, 1004, 668)
+
+  # player objects
   rect_player1 = Rect(20, 354, 10, 60)
+  rect_player2 = Rect(994, 354, 10, 60)
+
 
   while running:
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
     
+    keys = pygame.key.get_pressed() 
+    # move player1 
+    if keys[pygame.K_w]:
+      rect_player1.y -= 100 * dt
+    if keys[pygame.K_s]:
+      rect_player1.y += 100 * dt
+    rect_player1.clamp_ip(collision_surface)
+
+    # move player2
+    if keys[pygame.K_i]:
+      rect_player2.y -= 100 * dt
+    if keys[pygame.K_k]:
+      rect_player2.y += 100 * dt
+    rect_player2.clamp_ip(collision_surface)
+
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
-
 
     # draw the rectangle play
     pygame.draw.rect(screen, "white", pygame.Rect(rect), 2, 30)
@@ -40,19 +58,11 @@ def main():
     # draw the center line
     pygame.draw.line(screen, "white", (512, 50), (512, 755), 2)
 
-    # player1 box gameplay
+    # player1 box object 
     pygame.draw.rect(screen, player1_rgb, rect_player1 ,1, 2)
 
-
-  #  keys = pygame.key.get_pressed()
-  #  if keys[pygame.K_w]:
-  #    player_pos.y -= 300 * dt
-  #  if keys[pygame.K_s]:
-  #    player_pos.y += 300 * dt
-  #  if keys[pygame.K_a]:
-  #    player_pos.x -= 300 * dt
-  #  if keys[pygame.K_d]:
-  #    player_pos.x += 300 * dt
+    # player2 box object
+    pygame.draw.rect(screen, player2_rgb, rect_player2 ,1, 2)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
