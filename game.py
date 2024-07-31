320c5b895b3fa8dc5befbcae8c440ce072d1db1d
resfer3 <resfer333@gmail.com>
2024-07-31 05:45:17 -0600

    added better randomize ball and added TODO for scoreboard

--- game.py
+++ game.py
@@ -18,6 +18,8 @@ def main():
   clock = pygame.time.Clock()
   running = True
   dt = 0
+  # window name
+  pygame.display.set_caption("Pong")
 
 #  print(screen.get_width() / 2)
 #  print(screen.get_height() / 2)
@@ -36,12 +38,42 @@ def main():
   rect_player2 = Rect(994, 354, 10, 60)
 
   # ball starting object
-  ball = Circle(randint(400,600), 334, 10) 
+  ball = Circle(512,randint(300, 600), 10) 
 
   # start boolean
   start = False
 
+  # random starting direction
+  random_start = randint(1,4)
+
+  # font text
+  font = pygame.font.Font("freesansbold.ttf", 30)
+  # main text
+  text = font.render("Pong", True, (255, 255, 255), (0, 0, 0))
+  text_rect = text.get_rect()
+  text_rect.center = (screen.get_width() / 2, 25)
+
+  # scoreboard text
+
+#  player1_rgb = (66, 176, 245)
+#  player2_rgb = (245, 66, 81)
+
+  # score1 blue
+
+  score1 = 0
+  score1_text = font.render(str(score1), True, (66, 176, 245), (0, 0, 0))
+  score1_text_rect = score1_text.get_rect()
+  score1_text_rect.center = (251, 200)
+  # score1 blue
+  score2 = 0
+  score2_text = font.render(str(score2), True, (245, 66, 81), (0, 0, 0))
+  score2_text_rect = score2_text.get_rect()
+  score2_text_rect.center = (751, 200)
+
+  
+
   while running:
+
 
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -60,10 +92,25 @@ def main():
     # ball movement
 
     if start:
-      ball.y -= ball.vel_y
-      ball.x += ball.vel_x
+    
+      if random_start == 1:
+      # right up
+        ball.y -= ball.vel_y
+        ball.x += ball.vel_x
+      if random_start == 2:
+      # left down
+        ball.y += ball.vel_y
+        ball.x -= ball.vel_x
+      if random_start == 3:
+      # right down
+        ball.y += ball.vel_y
+        ball.x += ball.vel_x
+      if random_start == 4:
+      # left up
+        ball.y -= ball.vel_y
+        ball.x -= ball.vel_x
 
-      # bounce from wall, if player hit bounce, and keep direction
+        # bounce from wall, if player hit bounce, and keep direction
       if ball.y >= rect_player1.top and ball.y <= rect_player1.bottom and ball.x - ball.radius <= rect_player1.left and ball.x + ball.radius >= rect_player1.right:
         ball.vel_x *= -1
         player1_rgb = (255,255,255)
@@ -76,8 +123,18 @@ def main():
         print("hit")
       else:
         player2_rgb = (245, 66, 81) 
-      if ball.x - ball.radius < collision_surface.left or ball.x + ball.radius > collision_surface.right:
-        ball.vel_x *= -1
+
+       # if opposite wall hit, gain a point // TODO
+      if ball.x - ball.radius < collision_surface.left:
+        score2 += 1
+        start = False 
+        continue
+      if ball.x + ball.radius > collision_surface.right:
+        score1 += 1
+        start = False
+        continue
+      #if ball.x - ball.radius < collision_surface.left or ball.x + ball.radius > collision_surface.right:
+        #ball.vel_x *= -1
       if ball.y - ball.radius < collision_surface.top or ball.y + ball.radius > collision_surface.bottom:
         ball.vel_y *= -1
 
@@ -113,6 +170,11 @@ def main():
     # player2 box object
     pygame.draw.rect(screen, player2_rgb, rect_player2 ,1, 2)
 
+    # render text
+    screen.blit(text, text_rect)
+    screen.blit(score1_text, score1_text_rect)
+    screen.blit(score2_text, score2_text_rect)
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
@@ -122,6 +184,8 @@ def main():
     dt = clock.tick(60) / 1000
 
   pygame.quit()
+
+
 
 if __name__ == "__main__":
   main()
